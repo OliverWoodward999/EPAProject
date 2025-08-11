6b005825d98dbea36fb7361643942a57feedbb5a
OliverWoodward999 <makaveli18892@gmail.com>
2025-08-11 22:47:09 +0100

    Delete epaNew/src/api.js

--- epaNew/src/api.js
+++
@@ -1,6 +0,0 @@
-// src/api.js
-export const API_BASE =
-  (import.meta?.env && import.meta.env.VITE_API_BASE) ||
-  (window.location.hostname === 'localhost' ? 'http://localhost:5001' : '');
-
-export const apiUrl = (p) => (API_BASE ? `${API_BASE}${p}` : p);
